3283bb994684136ed335830c67abf495aea7358c
Matan Shidlov <91742238+mshidlov@users.noreply.github.com>
2022-02-06 16:06:04 +0200

    amplication-build-ckzbc8r16277501s6wzomv47z

--- admin-ui/src/App.tsx
+++ admin-ui/src/App.tsx
@@ -28,7 +28,7 @@ const App = (): React.ReactElement => {
   return (
     <div className="App">
       <Admin
-        title={"My app 1"}
+        title={"My app-1"}
         dataProvider={dataProvider}
         authProvider={jwtAuthProvider}
         theme={theme}
